5c5316f7f3d39f8fbe3df60c0c0fc0fb400b6749
Ingraham Girls Who Code <32528536+ingrahamgwc@users.noreply.github.com>
2017-11-29 14:49:46 -0800

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,6 @@
+These are the Arduino source files for the Ingraham GWC pancake robot 2017.
+
+Load these files into the Arduino SDK app running on a laptop.
+
+joystick control (push button to pour batter): pancakerDraw/pancakerDraw.ino 
+
